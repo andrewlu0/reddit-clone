57ddbd440bc1f1eb60f38cfe390010bd506fb0ae
Andrew Luo <32521085+andrewlu0@users.noreply.github.com>
2021-02-20 22:05:43 -0800

    :beers: login register page

---
+++ frontend/src/components/NavBar.tsx
@@ -0,0 +1,45 @@
+import { Box, Button, Flex, Link} from '@chakra-ui/react';
+import React from 'react'
+import NextLink from 'next/link'
+import { useMeQuery } from '../generated/graphql'
+
+interface NavBarProps {
+
+}
+
+export const NavBar: React.FC<NavBarProps> = ({}) => {
+  const [{data, fetching}] = useMeQuery();
+  let body = null;
+
+  if (fetching) {
+  } else if (!data?.me) {
+    body = (
+      <>
+        <NextLink href="/login">
+          <Link color="white" mr={4}>
+            Login
+          </Link>
+        </NextLink>
+        <NextLink href="/register">
+          <Link color="white" mr={4}>
+            Register
+          </Link>
+        </NextLink>
+      </>
+    );
+  } else {
+    body = (
+      <Flex>
+        <Box mr = {2} color = "white">{data.me.username}</Box>
+        <Button variant = "link">Logout</Button>
+      </Flex>
+    );
+  }
+    return (
+      <Flex bg = "#00B5D8" p = {4}>
+        <Box ml = {"auto"}>
+          {body}
+        </Box>
+      </Flex>
+    );
+}
