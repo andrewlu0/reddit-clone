78ae87f0a90e77c7a2f64df6581327b0a4410fdc
Andrew Luo <32521085+andrewlu0@users.noreply.github.com>
2021-02-25 00:07:55 -0800

    posts

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,20 @@
+import { withUrqlClient } from "next-urql";
 import { NavBar } from "../components/NavBar";
+import { usePostsQuery } from "../generated/graphql";
+import { createUrqlClient } from "../utils/createUrqlClient";
 
-const Index = () => (
-  <>
-    <NavBar />
-    <div>hello world</div>
-  </>
-);
-export default Index;
+const Index = () => {
+  const [{ data }] = usePostsQuery();
+  console.log(data);
+  return (
+    <>
+      <NavBar />
+      <div>hello world</div>
+      <br></br>
+      {!data ? (
+        <div>Loading...</div>
+      ): (data.posts.map((p) => <div key={p.id}>{p.title}</div>))}
+    </>
+  )
+};
+export default withUrqlClient(createUrqlClient, { ssr: true }) (Index);
