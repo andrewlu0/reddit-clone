7a5fb8282b1d8a810977c8e4f2232c6da93dafbd
Andrew Luo <32521085+andrewlu0@users.noreply.github.com>
2021-02-25 23:35:00 -0800

    util

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,15 +1,16 @@
-import { Box, Button, Flex, Link} from '@chakra-ui/react';
-import React from 'react'
-import NextLink from 'next/link'
-import { useLogoutMutation, useMeQuery } from '../generated/graphql'
+import { Box, Button, Flex, Link } from "@chakra-ui/react";
+import React from "react";
+import NextLink from "next/link";
+import { useLogoutMutation, useMeQuery } from "../generated/graphql";
+import { isServer } from "../utils/isServer";
 
-interface NavBarProps {
-
-}
+interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
-  const [{fetching: logoutFetching}, logout] = useLogoutMutation();
-  const [{data, fetching}] = useMeQuery();
+  const [{ fetching: logoutFetching }, logout] = useLogoutMutation();
+  const [{ data, fetching }] = useMeQuery({
+    pause: isServer(),
+  });
   let body = null;
 
   if (fetching) {
@@ -31,20 +32,24 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
   } else {
     body = (
       <Flex>
-        <Box mr = {2} color = "white">{data.me.username}</Box>
-        <Button onClick = {() => {
-          logout();
-        }}
-        isLoading = {logoutFetching}
-        variant = "link">Logout</Button>
+        <Box mr={2} color="white">
+          {data.me.username}
+        </Box>
+        <Button
+          onClick={() => {
+            logout();
+          }}
+          isLoading={logoutFetching}
+          variant="link"
+        >
+          Logout
+        </Button>
       </Flex>
     );
   }
-    return (
-      <Flex bg = "#00B5D8" p = {4}>
-        <Box ml = {"auto"}>
-          {body}
-        </Box>
-      </Flex>
-    );
-}
+  return (
+    <Flex bg="#00B5D8" p={4}>
+      <Box ml={"auto"}>{body}</Box>
+    </Flex>
+  );
+};
